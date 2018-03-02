36095b33b123388f9dc18450f9a2cd79fdc7f556
Vidmantas Kerbelis <vkerbelis@yahoo.com>
2018-03-02 15:44:25 +0200

    Set ModalProgressViewActivity.kt content as activity_modal_progress.xml

--- samples/src/main/kotlin/com/telesoftas/core/samples/widgets/modalprogress/ModalProgressViewActivity.kt
+++ samples/src/main/kotlin/com/telesoftas/core/samples/widgets/modalprogress/ModalProgressViewActivity.kt
@@ -2,9 +2,16 @@ package com.telesoftas.core.samples.widgets.modalprogress
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.telesoftas.core.samples.R
 
 class ModalProgressViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modal_progress)
+    }
+
     companion object {
         fun start(activity: Activity) {
             val intent = Intent(activity, ModalProgressViewActivity::class.java)
