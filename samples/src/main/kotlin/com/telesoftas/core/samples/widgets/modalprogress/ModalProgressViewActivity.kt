ad8bbc0a6a8a4fcc254c0779ece76c9d8c2ba5eb
Vidmantas Kerbelis <vkerbelis@yahoo.com>
2018-03-02 17:24:06 +0200

    Add progress color control for ModalProgressView#progressColor

--- samples/src/main/kotlin/com/telesoftas/core/samples/widgets/modalprogress/ModalProgressViewActivity.kt
+++ samples/src/main/kotlin/com/telesoftas/core/samples/widgets/modalprogress/ModalProgressViewActivity.kt
@@ -2,19 +2,26 @@ package com.telesoftas.core.samples.widgets.modalprogress
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
+import com.jaredrummler.android.colorpicker.ColorPickerDialog
+import com.jaredrummler.android.colorpicker.ColorPickerDialogListener
 import com.telesoftas.core.samples.R
 import kotlinx.android.synthetic.main.activity_modal_progress.*
 
-class ModalProgressViewActivity : AppCompatActivity() {
+class ModalProgressViewActivity : AppCompatActivity(), ColorPickerDialogListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modal_progress)
         launchButton.setOnClickListener {
             applyConfig()
             modalProgressView.showProgress()
+        }
+        colorView.setOnClickListener {
+            ColorPickerDialog.newBuilder().setColor(modalProgressView.progressColor)
+                    .show(this);
         }
         setUpInitialConfigState()
     }
@@ -24,12 +31,22 @@ class ModalProgressViewActivity : AppCompatActivity() {
         (delayTimeEditText as TextView).text = modalProgressView.progressDelay.toString()
         (animationTimeEditText as TextView).text =
                 modalProgressView.progressAnimationDuration.toString()
+        colorView.background = ColorDrawable(modalProgressView.progressColor)
     }
 
     private fun applyConfig() {
         modalProgressView.showProgressDelay = delayCheckBox.isChecked
         modalProgressView.progressDelay = delayTimeEditText.text.toString().toLong()
         modalProgressView.progressAnimationDuration = animationTimeEditText.text.toString().toLong()
+    }
+
+    override fun onColorSelected(dialogId: Int, color: Int) {
+        colorView.background = ColorDrawable(color)
+        modalProgressView.progressColor = color
+    }
+
+    override fun onDialogDismissed(dialogId: Int) {
+        // Do nothing
     }
 
     companion object {
